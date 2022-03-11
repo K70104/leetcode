1ba0a4c8f8e645df521005379f8e8fe366ef162c
chloe <74949102+K70104@users.noreply.github.com>
2022-03-11 16:35:16 +0800

    更新代码
    更新代码

---
+++ 20220310/src/TestDemo.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+public class TestDemo {
+    /*public List<Integer> preorder(Node root) {
+        // Node preRoot = inoder(root);
+    }*/
+
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
